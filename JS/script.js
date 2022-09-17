36f39a5051bd43d0ebaf1ee7a5106bffe12542bf
Bottonlina <95977923+bottonlina@users.noreply.github.com>
2022-09-17 03:35:44 +0000

    Introdução à Javascript

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+if(document.getElementById("nome").value estiver preenchido && document.getElementById("email").value estiver preenchido){
+ envie os dados 
+}else{
+  peça a usuária que preencha os campos nome e email
+}
