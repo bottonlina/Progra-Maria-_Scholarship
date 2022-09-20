41240ec5b2fdf6a791fc123d97f27e4768e77b15
Bottonlina <95977923+bottonlina@users.noreply.github.com>
2022-09-20 04:08:05 +0000

    Desafio de inclusão de campos

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,10 @@
 document.getElementById("botaoEnviar").addEventListener("click" , validaFormulario )
 
 function validaFormulario (){
-  if(document.getElementById("nome").value != '' && document.getElementById("email").value != ''){
+  if(document.getElementById("nome").value != '' && document.getElementById("telefone").value != '' && document.getElementById("email").value != ''){
     alert("Prontinho! Você receberá as novidades por e-mail!")
   }else{
-    alert("Por favor, preencha os campos de nome e email!")
+    alert("Por favor, preencha todos os campos do formulário!")
   }
 }
 
