b47d7db3753e101eb40606159ee5d90b49e97346
Bottonlina <95977923+bottonlina@users.noreply.github.com>
2022-09-20 04:10:48 +0000

    Ajuste de indentação

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,11 @@
 document.getElementById("botaoEnviar").addEventListener("click" , validaFormulario )
 
 function validaFormulario (){
-  if(document.getElementById("nome").value != '' && document.getElementById("telefone").value != '' && document.getElementById("email").value != ''){
-    alert("Prontinho! Você receberá as novidades por e-mail!")
+  if(document.getElementById("nome").value != '' && 
+     document.getElementById("telefone").value != '' && 
+     document.getElementById("email").value != ''){
+      alert("Prontinho! Você receberá as novidades por e- 
+      mail!")
   }else{
     alert("Por favor, preencha todos os campos do formulário!")
   }
